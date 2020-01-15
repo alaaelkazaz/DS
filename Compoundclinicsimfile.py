9ad606c759e4738af234d227e484d1428249cf9b
Alaa Elkzaz <52638420+leoelkzaz@users.noreply.github.com>
2020-01-15 12:38:36 +0200

    Update Compoundclinicsimfile.py

--- Compoundclinicsimfile.py
+++ Compoundclinicsimfile.py
@@ -16,10 +16,9 @@ class Queue:
 
 
 import random
-randage=random.randrange(20, 61)
+
 class Doctor :
     def __init__(self): #doctor class constructor
-        self.D_rate = randage/5
         self.currentpatient = None
         self.timeRemaining = 0
 
@@ -37,12 +36,12 @@ class Doctor :
 
     def Nextp(self,newp): # method to let the  next patient be the current patient and calculates the time he would take at the doctor's
         self.currentpatient = newp
-        self.timeRemaining = newp.getages()/self.D_rate
+        self.timeRemaining = newp.getages()//5
 
 class Patient :
     def __init__ (self, arrival):  #patient constructor with arrival time argument
         self.times= arrival
-        self.ages= randage
+        self.ages= random.randrange(20, 61)
 
     def getarrival (self):
         return  self.times
@@ -57,21 +56,21 @@ def ClinicSim(w):  # simulation for doctor clinic
     Doctor0 =Doctor ()
     patientQ= Queue() # creating The queue
     TimeWaiting=[]      # create  the waiting times alist
-    for currentsec in range(w):  # w is the time we enter ( 4 hours = 4*60*60 second)
-        if random.randrange(1,7)== 6 :                  #checks the probability of arrival of a patient at any given second
-            patient=Patient(currentsec)                     #creating a file for the patient
+    for currentsec in range(w):  # w is the time we enter ( 4 hours = 4*60 minute)
+        if random.randrange(1,7)== 6 :                  #checks the probability of arrival of a patient at any given minute
+            patient=Patient(currentmin)                     #creating a file for the patient
             patientQ.enqueue(patient)                       # adding the patient to the queue
 
         if not Doctor0.busy() and not patientQ.isEmpty() :  # check if the doctor is not busy and if there is a patient
             nextp=patientQ.dequeue()                        # delete patient from queue
-            TimeWaiting.append(nextp.waittime(currentsec))  #calculate the waiting time then add it to the list of waiting times
+            TimeWaiting.append(nextp.waittime(currentmin))  #calculate the waiting time then add it to the list of waiting times
             Doctor0.Nextp(nextp)                             #let the patient  enter to the doctor
 
-        Doctor0.tick()                                      #checks for patients still at the doctor
+       Doctor0.tick()                                      #checks for patients still at the doctor
     averagewait=sum(TimeWaiting)/len(TimeWaiting)       # calculate the average wait
     print ("Average Wait " , averagewait ," Min" , patientQ.size() , " tasks remaining.")
 
-#the simulation (Error generated)
+#the simulation 
 for i in range (40):
     ClinicSim(240)
 
